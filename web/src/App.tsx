fecbb9a97d453fe9872340e1636f5df6b5b89889
otoshigo <otatsuno27@gmail.com>
2025-08-09 23:37:07 +0900

    feat: 新規投稿ページを追加
    - NewPostPage.tsx を新規作成（プロジェクト投稿フォーム）
    - /new ルートを追加
    - ナビゲーションリンク間のスペーシングを修正

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -4,6 +4,7 @@ import IndexPage from './pages/IndexPage'
 import TestApiPage from './pages/TestApiPage'
 import LoginPage from './pages/LoginPage'
 import NotFoundPage from './pages/NotFoundPage'
+import NewPostPage from './pages/NewPostPage'
 
 import './App.css'
 
@@ -15,11 +16,14 @@ function App() {
           <Link to="/" style={{ marginRight: '20px', textDecoration: 'none', color: '#007bff' }}>
             Home
           </Link>
-          <Link to="/test-api" style={{ marginRight: '20px', textDecoration: 'none', color: '#007bff' }}>
-            Test API
-          </Link>
-          <Link to="/login" style={{ textDecoration: 'none', color: '#007bff' }}>
+          <Link to="/login" style={{ marginRight: '20px', textDecoration: 'none', color: '#007bff' }}>
             Login
+          </Link>
+          <Link to="/new" style={{ marginRight: '20px', textDecoration: 'none', color: '#007bff' }}>
+            投稿
+          </Link>
+          <Link to="/test-api" style={{ textDecoration: 'none', color: '#007bff' }}>
+            Test API
           </Link>
         </nav>
 
@@ -27,6 +31,7 @@ function App() {
           <Route path="/" element={<IndexPage />} />
           <Route path="/test-api" element={<TestApiPage />} />
           <Route path="/login" element={<LoginPage />} />
+          <Route path="/new" element={<NewPostPage />} />
           <Route path="*" element={<NotFoundPage />} />
         </Routes>
       </div>
